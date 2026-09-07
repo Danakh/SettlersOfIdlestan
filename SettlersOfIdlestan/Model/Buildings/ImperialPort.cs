6972f3632decee106439ec5c7bade1c6d351a15f
Danakh <danakh@ratzngodz.fr>
2026-09-07 06:57:45 +0200

    cout d'amélioration du port imperial

--- SettlersOfIdlestan/Model/Buildings/ImperialPort.cs
+++ SettlersOfIdlestan/Model/Buildings/ImperialPort.cs
@@ -25,7 +25,16 @@ public class ImperialPort : Building
     public long LastSeaportBuildTick { get; set; }
     public long GetAutoSeaportCooldownTicks() => 1000L;
 
-    public override ResourceSet GetUpgradeCost(int level) => new();
+    // Même progression que les autres bâtiments uniques à automatisation (Académie, Guilde des
+    // Bâtisseurs) : ×2 le coût de construction au niveau 2, puis ×3,5, ×5,5 et ×8.
+    public override ResourceSet GetUpgradeCost(int level) => level switch
+    {
+        2 => new ResourceSet { { Resource.Wood, 400 }, { Resource.Brick, 200 }, { Resource.Stone, 200 }, { Resource.Gold, 60 } },
+        3 => new ResourceSet { { Resource.Wood, 700 }, { Resource.Brick, 350 }, { Resource.Stone, 350 }, { Resource.Gold, 100 } },
+        4 => new ResourceSet { { Resource.Wood, 1100 }, { Resource.Brick, 550 }, { Resource.Stone, 550 }, { Resource.Gold, 160 } },
+        5 => new ResourceSet { { Resource.Wood, 1600 }, { Resource.Brick, 800 }, { Resource.Stone, 800 }, { Resource.Gold, 240 } },
+        _ => new ResourceSet()
+    };
 
     public override bool IsBuildingAvailableForCity(IslandMap.IslandMap map, IBuildingContext city, Model.Civilization.Civilization? civ)
     {
